e276ffe0b0365020cf3fb477c558cd11fe348076
ducknowledges <ducknowledges@gmail.com>
2023-05-06 13:29:18 +0500

    feat(spring-boot-web-vue-app-maven-starter): add app starter

---
+++ spring-boot-web-vue-app-maven-starter/ui/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<script>
+
+</script>
+
+<template>
+
+  <h1>App</h1>
+
+</template>
---
+++ spring-boot-web-vue-app-maven-starter/ui/src/router/index.js
@@ -0,0 +1,14 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'view',
+      component: {},
+    }
+  ]
+})
+
+export default router
